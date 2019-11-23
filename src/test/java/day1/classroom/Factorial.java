119de11472a9637172a121680a72e9d7058d920c
new <new@new-PC>
2019-11-23 19:42:57 +0530

    sriazgit

---
+++ src/test/java/day1/classroom/Factorial.java
@@ -0,0 +1,60 @@
+package day1.classroom;
+
+public class Factorial {
+	
+	/*
+	 * Goal: Find the Factorial of a given number
+	 * 
+	 * input: 5
+	 * output: 5*4*3*2*1 = 120
+	 * 
+	 * Shortcuts:
+	 * 1) Print : type: syso, followed by: ctrl + space + enter
+	 * 2) To create a 'for' loop: type 'for', followed by ctrl + space + down arrow + enter
+	 *   
+	 * What are my learnings from this code?
+	 * 1)
+	 * 2)
+	 * 3) 
+	 * 
+	 */	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Finding the factorial of a given number");
+		int n = 5;
+		int i ;
+		int fact = 0;
+				
+				while (i>=n)
+				{
+					fact = fact+(n*i);
+					i = i-1;
+					
+							
+				}
+		System.out.println("is factorial for given number is"+fact);
+		
+		 
+		
+
+		// Declare your input number
+		
+		
+		// Declare and initialize an integer variable by name: fact
+		
+
+		// Iterate from 1 to your input (tip: using loop concept)
+		
+
+			// Within the loop: Multiply fact with the iterator variable (Tip: Assign it to the 'fact' variable)
+			
+
+		//End of loop
+		
+
+		// Print factorial (fact)
+	
+	}
+
+}
---
+++ src/test/java/day1/classroom/Mobile.java
@@ -0,0 +1,19 @@
+package day1.classroom;
+
+public class Mobile {
+	public String makeCall(String Name) {
+		return "Name";
+
+	}
+
+	public String sendSMS(String Content) {
+
+		return "Content";
+
+	}
+
+	public boolean shutDown() {
+		return true;
+	}
+
+}
